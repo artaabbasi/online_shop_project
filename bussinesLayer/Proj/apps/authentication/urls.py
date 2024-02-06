c634ab38488bf412132e6af87a93666d58753fcf
arta <arta1382@gmail.com>
2024-02-06 19:34:10 +0330

    add docker files.

--- bussinesLayer/Proj/apps/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
-
-urlpatterns = [
-    path('login/', views.base_login),
-    path('register/', views.register)
-]
